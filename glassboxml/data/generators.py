7cebafda7dfd8cfc66b23ad1c0471e708c0c6b30
Iskken <iimanaliev312@gmail.com>
2026-03-16 20:47:35 +0100

    Implement classification dataset generation

--- glassboxml/data/generators.py
+++ glassboxml/data/generators.py
@@ -44,3 +44,51 @@ def generate_regression_dataset(
     y = X @ w_true + b_true + noise
 
     return X, y, w_true, b_true
+
+def generate_classification_dataset(
+    w_true,
+    b_true = 0.0,
+    n_samples=1000,
+    noise_std=0.01,
+    random_seed=42
+):
+    """
+    Generate synthetic classification dataset.
+
+    y = sigmoid(Xw + b + noise)
+
+    Parameters
+    ----------
+    w_true : array-like
+        Ground truth weight vector.
+    n_samples : int
+        Number of samples to generate.
+    noise_std : float
+        Standard deviation of Gaussian noise.
+    random_seed : int
+        For reproducibility.
+
+    Returns
+    -------
+    X : ndarray
+    y : ndarray
+    """
+
+    np.random.seed(random_seed)
+
+    w_true = np.array(w_true)
+    n_features = len(w_true)
+
+    #Generates input values with normal distribution where mean = 0, variance = 1
+    X = np.random.randn(n_samples, n_features)
+
+    #Generates one noise value per sample
+    noise = np.random.normal(0, noise_std, size=n_samples)  
+
+    #Calculates true values of y: X multiplies true weights, @ is a matrix multiplication operator
+    linear_output = X @ w_true + b_true + noise
+
+    #Applies sigmoid function to get probabilities and then thresholds at 0.5 to get binary labels
+    y = (1 / (1 + np.exp(-linear_output)) >= 0.5).astype(int)
+
+    return X, y, w_true, b_true
---
+++ tests/test_logistic_regression.py
@@ -0,0 +1,5 @@
+import numpy as np
+from glassboxml.models.logistic_regression import LogisticRegression
+
+def test_logistic_regression_convergence():
+    
